05ea3acddfafb4cf72757c421df6b95059e88f72
Francesco Lovascio <francesco.lovascio@ens-lyon.fr>
2022-05-25 15:00:34 +0200

    broken for loop

--- roots.hxx
+++ roots.hxx
@@ -18,7 +18,7 @@ namespace complexRootFind{
     template <class T, class... argType>
     std::complex<T> solve(std::complex<T> & x_0, std::function<std::complex<T>(std::tuple<argType...>,std::complex<T>)> & f,  std::function<std::complex<T>(std::tuple<argType...>,std::complex<T>)> & f_prime, std::tuple<argType...> & args, T & threshold){
         std::complex<T> x_n = x_0; 
-        for(int i=0;i < 1000){
+        for(int i=0;i < 1000;++i){
             x_n=newtonStep(x_n,f,f_prime,args);
             if(std::abs(f(args,x_n))<threshold){
                 return x_n;
